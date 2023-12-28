47f7986a91f63e1759b62d35c296ef3167e81ebb
710242 <jakeinccu@gmail.com>
2023-12-28 23:28:41 +0800

    fix sparse table range index error

--- sparsetable/sparsetable.c
+++ sparsetable/sparsetable.c
@@ -5,18 +5,18 @@
 struct SparseTable *new_sparsetable(int nums[], int len) {
   struct SparseTable *st = malloc(sizeof(struct SparseTable));
 
-  int P = floor(log(len) / log(2));
+  int P = floor(log2(len));
 
   st->n = len;
   st->p = P;
 
   st->table = malloc(sizeof(int *) * P);
 
-  for (int i = 0; i < P; i++) {
+  for (int i = 0; i <= P; i++) {
     st->table[i] = malloc(sizeof(int) * (len));
     // fill first row with origin datas
     if (i == 0) {
-      for (int j = 0; j < len - 1; j++) {
+      for (int j = 0; j < len; j++) {
         st->table[i][j] = nums[j];
       }
     }
@@ -26,10 +26,11 @@ struct SparseTable *new_sparsetable(int nums[], int len) {
 }
 
 void build_sparsetable(struct SparseTable *st, int (*dp_func)(int, int)) {
-  for (int i = 1; i < st->p; i++) {
-    for (int j = 0; j <= st->n - (1 << i); j++) {
+  for (int i = 1; i <= st->p; i++) {
+    for (int j = 0; j < st->n - (1 << (i - 1)); j++) {
       int left = st->table[i - 1][j];
-      int right = st->table[i - 1][j + (1 << i) - 1];
+      int right = st->table[i - 1][j + (1 << (i - 1))];
+      printf("%d %d\n", j, j + (1 << (i - 1)));
       st->table[i][j] = dp_func(left, right);
     }
   }
@@ -39,17 +40,37 @@ int min_func(int left, int right) { return left < right ? left : right; }
 
 void print_table(struct SparseTable *st) {
   printf("n: %d, p: %d\n", st->n, st->p);
-  for (int i = 0; i < st->p; i++) {
+  int prev_len = st->n;
+  for (int i = 0; i <= st->p; i++) {
     if (i == 0) {
-      for (int j = 0; j < st->n; j++) {
-        printf("%d, ", st->table[i][j]);
+      for (int j = 0; j < prev_len; j++) {
+        printf("%2d, ", st->table[i][j]);
       }
       printf("\n");
+      prev_len = st->n - (1 << i);
       continue;
     }
-    for (int j = 0; j <= st->n - (1 << i); j++) {
-      printf("%d, ", st->table[i][j]);
+    for (int j = 0; j < prev_len; j++) {
+      printf("%2d, ", st->table[i][j]);
     }
     printf("\n");
+    prev_len -= (1 << i);
   }
+}
+
+int minQuery(struct SparseTable *st, int l, int r) {
+  // NOTE: this is LEN so + 1 is essential
+  int len = r - l + 1;
+  int p = floor(log2(len));
+  int k = 1 << p;
+  return min_func(st->table[p][l], st->table[p][r - k + 1]);
+}
+
+int cascadingminQuery(struct SparseTable *st, int l, int r) {
+  int min = -1000000000;
+  for (int p = log2(r - l + 1); l <= r; p = log2(r - l + 1)) {
+    min = min_func(min, st->table[p][l]);
+    l += (1 << p);
+  }
+  return min;
 }
--- sparsetable/sparsetable.h
+++ sparsetable/sparsetable.h
@@ -13,7 +13,5 @@ void build_sparsetable(struct SparseTable *st, int (*dp_func)(int, int));
 int min_func(int, int);
 void print_table(struct SparseTable *st);
 
-// TODO:
-// given range: l to r, find the dp_func num in the range
-// int minQuery(struct SparseTable* st, int l, int r);
-// int cascadingQuery(struct SparseTable*, int l, int r);
+int minQuery(struct SparseTable *st, int l, int r);
+int cascadingminQuery(struct SparseTable *st, int l, int r);
